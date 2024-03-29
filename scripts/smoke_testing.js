aad1f31518398c56a046e8124da89977d006505f
Bruno Santos <brunogsantoss@outlook.com>
2024-03-29 13:06:37 -0300

    Smoke testing

--- scripts/smoke_testing.js
+++ scripts/smoke_testing.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep, group } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+// import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
   vus: 5, // Key for Smoke test. Keep it at 2, 3, max 5 VUs
@@ -9,6 +9,7 @@ export const options = {
 
 export default function () {
     const BASE_URL = 'https://mockapi.io/projects/648cf7d98620b8bae7ed81b8';
+    const id = 1;
 
     group('GET Users', function () {
         const res = http.get(`${BASE_URL}/Users`);
@@ -17,10 +18,31 @@ export default function () {
             'Status Code 200': (r) => r.status === 200
         })
     });
+
+    group('GET Users Id', function () {
+      const res = http.get(`${BASE_URL}/Users/${id}`);
+  
+      check(res,{
+          'Status Code 200': (r) => r.status === 200
+      })
+    });
+
+    group('PUT Users', function () {
+      const url = `https://mockapi.io/projects/648cf7d98620b8bae7ed81b8/Users/5`;
+
+      const headers = { 'Content-Type': 'application/json' };
+      const data = { "Nome": "Bert" };
+
+      const res = http.put(url, JSON.stringify(data), { headers: headers });
+  
+      check(res,{
+          'Status Code 200': (r) => r.status === 200
+      })
+    });
 };
 
-export function handleSummary(data) {
-    return {
-      "Relatorio_k6.html": htmlReport(data),
-    };
-}
+// export function handleSummary(data) {
+//     return {
+//       "Relatorio_k6.html": htmlReport(data),
+//     };
+// }
